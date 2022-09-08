b36de1c6c1d82ddfcb2f2ef2c8703e12593d0ecf
EShip <shipelin.e@yandex.ru>
2022-09-09 00:14:36 +0300

    ПР4 v.2.0

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 const editButton  	 = document.querySelector('.ProfileInfo__EditButton');
-const closeButton 	 = document.querySelector('.popup__close');
+const closeButton 	 = document.querySelector('.edit_profile_form__close');
 const popup		  	 = document.querySelector('.popup');
 /*поля в шапке*/
 let   nameInput   	 = document.querySelector('.ProfileInfo__header');
 let   jobInput 	  	 = document.querySelector('.ProfileInfo__description');
 /*поля в модалке*/
-let   modalNameInput = document.querySelector('.popup__name');
-let   modalJobInput  = document.querySelector('.popup__label');
-let   popupContainer = document.querySelector('.popup__container');
+let   modalNameInput = document.querySelector('.edit_profile_form__input_field_name');
+let   modalJobInput  = document.querySelector('.edit_profile_form__input_field_label');
+let   popupContainer = document.querySelector('.edit_profile_form');
 //
 // функция отображения popup
 //
@@ -23,7 +23,7 @@ const closePopup = () => {
 	popup.classList.remove('popup_opened');
 }
 //
-// Обработчик «отправки» формы, хотя покаона никуда отправляться не будет
+// Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
 //
 const formSubmitHandler = (evt) => {
     evt.preventDefault();
--- script/script.js
+++
@@ -1,5 +0,0 @@
-
-	let al = () => {alert(123);}
-	function ale(){
-		return(alert(222));
-	}
